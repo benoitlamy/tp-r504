80a5a666b5b68169596026b7017566651cafe409
Benoit LAMY <benoit.lamy27@gmail.com>
2023-09-07 16:21:28 +0200

    update test.py fonctions.py and prog.py

---
+++ TP1/fonctions.py
@@ -0,0 +1,13 @@
+def puissance(a,b):
+	if not type(a) is int:
+		raise TypeError("Only integers are allowed")
+	if not type(b) is int:
+		raise TypeError("Only integers are allowed")
+	return a ** b
+	
+#print ("fonctions puissance")
+#if not type(vars) is int:
+#	resultat = (int(input()))**(int(input()))
+#	print (resultat)
+#raise TypeError("Only integers are allowed")
+
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,12 @@
+import fonctions as f
+
 print("Hello, World!")
+print ("Veuillez entrer un nombre positif quelconque : "),
+V1 = int(input())
+print("V1=", V1**2)
+
+a=int(input())
+b=int(input())
+V2 = f.puissance(a,b)
+print ("v2=", V2)
+
