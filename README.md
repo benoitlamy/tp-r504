6a7a5e4b44000cba90351371ed8406ff77680662
benoitlamy <benoit.lamy27@gmail.com>
2023-09-27 17:03:48 +0200

    maj1.2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # tp-r504
 
 ![text ALT] https://github.com/benoitlamy/tp-r504/actions/workflows/pytest.yml/badge.svg
+
+[![ALT textGH] https://github.com/benoitlamy/tp-r504/actions/workflows/pytest.yml/badge.svg] https://github.com/benoitlamy/tp-r504/actions
--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,10 +1,9 @@
 def puissance(a,b):
-	if not type(a) is int:
-		raise TypeError("Only integers are allowed")
-	if not type(b) is int:
-		raise TypeError("Only integers are allowed")
-
-	return a ** b
+	for a ** b
+		if not type(a) is int:
+			raise TypeError("Only integers are allowed")
+		if not type(b) is int:
+			raise TypeError("Only integers are allowed")
 	
 #print ("fonctions puissance")
 #if not type(vars) is int:
